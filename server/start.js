59905ad20b1252ad60c049567630f6c07a16d7bc
Boitumelo Kotane <kaiineoyoung@gmail.com>
2024-08-14 15:31:35 +0200

    readme

---
+++ server/start.js
@@ -0,0 +1,12 @@
+removeEventListener('dotenv').config() // to access enviroonment variables
+const express = require('express');
+const app = express(); // create an instance of express web server
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+}); //default endpoint
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on https://localhost:${PORT}`);
+})
