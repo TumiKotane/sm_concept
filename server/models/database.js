fb16aba094c7cfeb0b7192bb864b3196e2d636e2
Boitumelo Kotane <kaiineoyoung@gmail.com>
2024-08-14 23:30:58 +0200

    ONE TO MARY RELATIONAL TABLE

--- server/models/database.js
+++ server/models/database.js
@@ -31,16 +31,22 @@ const db = {}
 db.Sequelize = Sequelize
 db.sequelize = sequelize
 
+db.users = require('./userModel.js')(sequelize, DataTypes)
+db.posts = require('./postModel.js')(sequelize, DataTypes)
+db.comments = require('./commentModel.js')(sequelize, DataTypes)
+
 db.sequelize.sync({ force: false })
 .then(() => {
     console.log('Re-sync done!')
 })
 
 //1 to Many relation
-//db.products.hasMany(db.review)
-
-
-
+db.users.hasMany(db.posts)
+db.posts.belongsTo(db.users)
+db.posts.hasMany(db.comments)
+db.comments.belongsTo(db.posts)
+db.users.hasMany(db.comments)
+db.comments.belongsTo(db.users)
 
 
 module.exports = db
