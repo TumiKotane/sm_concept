e40ea0c43e98d6ef24363ab1abbe35e0800be83a
Boitumelo Kotane <kaiineoyoung@gmail.com>
2024-08-15 00:33:01 +0200

    tester endpoints

--- server/index.js
+++ server/index.js
@@ -6,25 +6,26 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 }); //default endpoint
 
-app.get('/SignUp', (req, res) => {
-    res.send('This will be the sign up page');
-}); //default endpoint
+//Tester endpointsS
+//app.get('/SignUp', (req, res) => {
+// res.send('This will be the sign up page');
+//}); //default endpoint
 
-app.get('/LogIn', (req, res) => {
-    res.send('This will be the login page');
-}); //default endpoint
+//app.get('/LogIn', (req, res) => {
+ //   res.send('This will be the login page');
+//}); //default endpoint
 
-app.get('/Home', (req, res) => {
-    res.send('This will be the home page');
-}); //default endpoint
+//app.get('/Home', (req, res) => {
+ //   res.send('This will be the home page');
+//}); //default endpoint
 
-app.get('/Profile', (req, res) => {
-    res.send('This will be the profile page');
-}); //default endpoint
+//app.get('/Profile', (req, res) => {
+//    res.send('This will be the profile page');
+//}); //default endpoint
 
-app.get('/Comments', (req, res) => {
-    res.send('This will be the comments page');
-}); //default endpoint
+//app.get('/Comments', (req, res) => {
+//    res.send('This will be the comments page');
+//}); //default endpoint
 
 app.get('/Inbox', (req, res) => {
     res.send('This will be the inbox page');
