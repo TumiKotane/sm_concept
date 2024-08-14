f2e763f796b13e7dc2f44503944882d14c2cfeca
Boitumelo Kotane <kaiineoyoung@gmail.com>
2024-08-14 23:16:21 +0200

    adjusted database relation

---
+++ server/models/postModel.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize")
+const { sequelize } = require("./database")
+
+module.exports = (sequelize, DataTypes) => {
+    const Post = sequelize.define('post', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+    return Post
+}
---
+++ server/models/userModel.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require("sequelize")
+const { sequelize } = require("./database")
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('user', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+    return User
+}
